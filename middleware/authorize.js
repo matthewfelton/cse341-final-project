1b13b40c8e2a97b27907c5e7c239104b3578243a
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2024-03-11 18:20:26 -0500

    added more and modified file for oauth

---
+++ middleware/authorize.js
@@ -0,0 +1,27 @@
+// Checks for access for modifing data
+const checkLogStatus = (req, res, next) => {
+    if (!req.oidc.isAuthenticated()) {
+        return res.status(401).send({
+            error: "Not authorized to change data without login.",
+            login: "https://cse341-final-project-my43.onrender.com/login",
+        });
+    }
+    next();
+};
+
+// Check for access for viewing and modifing data
+const checkLogAll = (req, res, next) => {
+    if (!req.oidc.isAuthenticated()) {
+        return res.status(401).send({
+            error: "Not authorized to view or change data without login.",
+            login: "https://cse341-final-project-my43.onrender.com/login",
+        });
+    }
+    next();
+};
+
+
+module.exports = { 
+    checkLogStatus,
+    checkLogAll 
+};
--- server.js
+++ server.js
@@ -8,6 +8,23 @@ const mongodb = require('./db/connection');
 const port = process.env.port || 8080;
 
 
+// config for 0auth
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 
 
@@ -27,23 +44,6 @@ process.on('uncaughtException', (err, origin) => {
 
 const { auth } = require('express-openid-connect');
 
-// config for 0auth
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: process.env.SECRET,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    issuerBaseURL: process.env.ISSUER_BASE_URL
-};
-
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
 
 mongodb.initDb((err) => {
     if (err) {
