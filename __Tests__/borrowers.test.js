fad4119dacb25ee573824d74a8bb8c12605f0ca7
annmarieinlr <wor20005@byui.edu>
2024-03-25 13:09:48 -0500

    a commit

--- __Tests__/borrowers.test.js
+++ __Tests__/borrowers.test.js
@@ -1,7 +1,39 @@
+//From the documentation
+
+// const {MongoClient} = require('mongodb');
+
+// describe('insert', () => {
+//   let connection;
+//   let db;
+
+//   beforeAll(async () => {
+//     connection = await MongoClient.connect(globalThis.__MONGO_URI__, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     db = await connection.db(globalThis.__MONGO_DB_NAME__);
+//   });
+
+//   afterAll(async () => {
+//     await connection.close();
+//   });
+
+//   it('should insert a doc into collection', async () => {
+//     const users = db.collection('users');
+
+//     const mockUser = {_id: 'some-user-id', name: 'John'};
+//     await users.insertOne(mockUser);
+
+//     const insertedUser = await users.findOne({_id: 'some-user-id'});
+//     expect(insertedUser).toEqual(mockUser);
+//   });
+// });
+
+//From Instructor code:
 const app = require('../borrowers')
 const supertest = require('supertest');
 const { expect } = require('@jest/globals');
-const request = supertest(borrowers)
+const request = supertest(app)
 
 
 describe('Test Handlers', () => {
@@ -10,9 +42,11 @@ describe('Test Handlers', () => {
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
+    //Questions for tutor. 
+    //1. Should /users be /getAll?
 
-    test('responds to /users', async () => {
-        const res = await request.get('/users');
+    test('responds to /borrowers', async () => {
+        const res = await request.get('/borrowers');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
