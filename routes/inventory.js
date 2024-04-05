f569396da3a55dfec9982fc22befe79afe39fce4
matthewfelton <77081600+matthewfelton@users.noreply.github.com>
2024-04-04 21:19:06 -0500

    fixing bug

--- routes/inventory.js
+++ routes/inventory.js
@@ -22,27 +22,3 @@ router.delete('/:id', /* OAuth.checkLogStatus, */ inventoryController.deleteInve
 
 // export module for rest of code to use
 module.exports = router;
-// inventory route file
-
-const express = require('express');
-const router = express.Router();
-
-
-const inventoryController = require('../controllers/inventory')
-const validation = require('../middleware/validate');
-const OAuth = require("../middleware/authorize");
-
-// Validation and Oauth added after testing of routes
-
-router.get('/', inventoryController.getAll);
-
-router.get('/:id', inventoryController.getSingle);
-
-router.post('/', /*OAuth.checkLogStatus,*/ validation.saveInventory, inventoryController.newInventory);
-
-router.put('/:id', /*OAuth.checkLogStatus,*/ validation.saveInventory, inventoryController.updateInventory);
-
-router.delete('/:id', /*OAuth.checkLogStatus,*/ inventoryController.deleteInventory);
-
-// export module for rest of code to use
-module.exports = router;
